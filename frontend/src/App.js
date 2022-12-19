d4d6ab057e315f06b59c0786f890332a8efde184
Robert Castro <robert@FSOFTWARE04.rtvc.gov.co>
2022-12-18 23:57:33 -0500

    Calculo GET

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import IMC from './components/IMC';
 import { Route, Routes  } from 'react-router-dom';
+import GETForm from './components/GET';
 
 
 const Home = () => <h1>Home</h1>
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}  />
         <Route path='/blog/calcular-imc-indice-de-masa-corporal/' element={<IMC />}  />
+        <Route path='/blog/getform' element={<GETForm />} />
 
       </Routes>
     </div>
